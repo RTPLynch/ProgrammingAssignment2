355d074b77dc6234cfef3c4058b922a65be50026
RTPLynch <rtplynch@hotmail.com>
2014-07-26 14:20:14 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## RTPLynch
 
-## Write a short comment describing this function
+## makceCacheMatrixList is a function that takes a matrix as argument
+## and returns a list object which contains four subobjects :
+## get (which returns the input matrix argument)
+## set which allows one to store a new matrix
+## setInv which allows one to store an Inverse matrix
+## and getInv which retrieves the value of the Inverse of the input argument
+## but only if done after first calling cacheInv , otherwise it returns null
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrixList <- function(x = matrix()) {
+      I <- NULL
+      set <- function(y) {
+            x <<- y
+            I <<- NULL
+      }
+      get <- function() x
+      setInv <- function(Inv) I <<- Inv
+      getInv <- function() I
+      list(set = set, get = get,
+           setInv = setInv,
+           getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheInv is a function that takes a matrix as argument
+## and returns its inverse
+## Where the inverse has already been computed it returns the value stored in cache 
+## Where the inverse has not yet been computed , it calculates the inverse
+## then calls the makeCacheMatrixList function's output argument setInv to store
+## the value of the Inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheInv <- function(x, ...) {
+      I <- x$getInv()
+      if(!is.null(I)) {
+            message("getting cached data")
+            return(I)
+      }
+      data <- x$get()
+      I <- solve(data, ...)
+      x$setInv(I)
+      I
 }
